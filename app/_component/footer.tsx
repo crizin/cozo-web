7812e627b1f5c9cefa9c6a87fd6124af3d671d2c
JaeYong Lee <crizin@gmail.com>
2023-08-27 00:47:42 +0900

    Change about link text

--- app/_component/footer.tsx
+++ app/_component/footer.tsx
@@ -6,7 +6,7 @@ import styles from './footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Image src="/icon.svg" width={16} height={16} alt="Logo" /> {moment().year()}. cozo / <Link href="/about">사이트 소개</Link>
+      <Image src="/icon.svg" width={16} height={16} alt="Logo" /> {moment().year()}. cozo / <Link href="/about">about</Link>
     </footer>
   );
 }
