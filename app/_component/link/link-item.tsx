c19f426cd6f50f2909a02320567defbb811cbc45
JaeYong Lee <crizin@gmail.com>
2023-09-06 13:03:40 +0900

    Modify new window on external link

--- app/_component/link/link-item.tsx
+++ app/_component/link/link-item.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import ExternalImage from '@/app/_component/fragment/external-image';
 import Favicon from '@/app/_component/fragment/favicon';
+import { GlobalVariableContext } from '@/app/_component/global-variable-context';
 import { Link } from '@/app/_lib/model';
 import Utils from '@/app/_lib/utils';
+import { useContext } from 'react';
 import styles from './link-item.module.scss';
 
 export default function LinkItem({ link }: { link: Link }) {
+  const { state } = useContext(GlobalVariableContext);
+
   return (
-    <a href={link.url} rel="nofollow" className={styles.link}>
+    <a href={link.url} rel="nofollow" className={styles.link} target={state.useNewWindow ? '_blank' : '_self'}>
       <span className={`${styles.title} ellipsis`}>
         <Favicon faviconUrl={link.faviconUrl} size={16} />
         {link.title ? link.title : Utils.decorateUrl(link.url)}
