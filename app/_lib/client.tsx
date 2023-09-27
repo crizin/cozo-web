a4b2f712508638fbe033d6e80505e493c9551a65
JaeYong Lee <jaeyong.crizin@navercorp.com>
2023-09-27 16:00:18 +0900

    Use only one API Endpoint

--- app/_lib/client.tsx
+++ app/_lib/client.tsx
@@ -1,7 +1,7 @@
 import { Article, Board, Link, Page, Response, SearchResult, TagTrend } from '@/app/_lib/model';
 
 export async function getBoards(): Promise<Response<Board[]>> {
-  return fetch(`${process.env.API_ENDPOINT}/boards`, { next: { revalidate: 3600 } }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/boards`, { next: { revalidate: 3600 } }).then((response) => validateResponse(response));
 }
 
 export async function getBoard(id: number) {
@@ -24,33 +24,33 @@ export async function getBoardMap(): Promise<Record<string, Board>> {
 }
 
 export async function getArticles(cursor = 0): Promise<Response<Page<Article[], number>>> {
-  return fetch(`${process.env.API_ENDPOINT}/articles?cursor=${cursor}`, { cache: 'no-store' }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/articles?cursor=${cursor}`, { cache: 'no-store' }).then((response) => validateResponse(response));
 }
 
 export async function getBoardArticles(boardId: number, cursor = 0): Promise<Response<Page<Article[], number>>> {
-  return fetch(`${process.env.API_ENDPOINT}/boards/${boardId}/articles?cursor=${cursor}`, { cache: 'no-store' }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/boards/${boardId}/articles?cursor=${cursor}`, { cache: 'no-store' }).then((response) => validateResponse(response));
 }
 
 export async function getLinks(page = 1): Promise<Response<Page<Link[], number>>> {
-  return fetch(`${process.env.API_ENDPOINT}/links?page=${page}`, { cache: 'no-store' }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/links?page=${page}`, { cache: 'no-store' }).then((response) => validateResponse(response));
 }
 
 export async function getKeywordLastDate(): Promise<Response<string>> {
-  return fetch(`${process.env.API_ENDPOINT}/keywords/last-date`, { cache: 'no-store' }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/keywords/last-date`, { cache: 'no-store' }).then((response) => validateResponse(response));
 }
 
 export async function getKeywords(date: string): Promise<Response<Page<TagTrend[], string>>> {
-  return fetch(`${process.env.API_ENDPOINT}/keywords/${date}`, { cache: 'no-store' }).then((response) => validateResponse(response));
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/keywords/${date}`, { cache: 'no-store' }).then((response) => validateResponse(response));
 }
 
 export async function getSearchResults(keyword: string, page = 1): Promise<Response<Page<SearchResult, number>>> {
-  return fetch(`${process.env.API_ENDPOINT}/search?keyword=${encodeURIComponent(keyword)}&page=${page}`, { cache: 'no-store' }).then((response) =>
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/search?keyword=${encodeURIComponent(keyword)}&page=${page}`, { cache: 'no-store' }).then((response) =>
     validateResponse(response),
   );
 }
 
 export async function getSearchResultsByBoard(boardId: number, keyword: string, page = 1): Promise<Response<Page<SearchResult, number>>> {
-  return fetch(`${process.env.API_ENDPOINT}/search/board/${boardId}?keyword=${encodeURIComponent(keyword)}&page=${page}`, { cache: 'no-store' }).then(
+  return fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/search/board/${boardId}?keyword=${encodeURIComponent(keyword)}&page=${page}`, { cache: 'no-store' }).then(
     (response) => validateResponse(response),
   );
 }
